f1e89d509e9eb0ce2d19a89fc093a5643efff5ff
郭斌勇 <guobinyong@qq.com>
2023-06-09 15:51:47 +0800

    feat: 雾点材质

--- demoList.js
+++ demoList.js
@@ -116,29 +116,29 @@ export default [
   },
   //VolumeMesh
   {
-    title: "体积材质",
+    title: "立体材质",
     key: "VolumeMesh",
     children: [
       {
-        title: "梯度体积材质",
+        title: "梯度立体材质",
         demoLink: "./page/volumeMesh/gradient/index.html",
       },
       {
-        title: "图像体积材质",
+        title: "图像立体材质",
         demoLink: "./page/volumeMesh/image/index.html",
       },
       {
-        title: "球形雾体积材质",
+        title: "球形雾立体材质",
         demoLink: "./page/volumeMesh/sphere/index.html",
       },
-      // {
-      //   title: "颜色雾点材质",
-      //   demoLink: "./page/volumeMesh/colorFogPoints/index.html",
-      // },
-      // {
-      //   title: "梯度雾点材质",
-      //   demoLink: "./page/volumeMesh/gradientFogPoints/index.html",
-      // },
+      {
+        title: "颜色雾点材质",
+        demoLink: "./page/volumeMesh/colorFogPoints/index.html",
+      },
+      {
+        title: "梯度雾点材质",
+        demoLink: "./page/volumeMesh/gradientFogPoints/index.html",
+      },
       // 梯度雾点材质,
     ],
   },
